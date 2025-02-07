73695dd1ccc6bc24d27ed962c428094cf8ff066e
Joseeaantoniio22 <jfergue055@g.educaand.es>
2025-02-07 01:11:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let lupa = document.getElementById("lupa");
+let buscador = document.getElementById("buscador");
+buscador.addEventListener("mouseenter", ()=>{
+    buscador.style.width=("150px")
+    buscador.style.height=("30px")
+    buscador.style.borderRadius=("10px")
+    buscador.style.background=("white")
+    buscador.style.transition=("all 0.5s linear")
+})
 
-lupa.addEventListener("click", function(){
-    
+buscador.addEventListener("mouseleave", () => {
+    buscador.style.removeProperty("width");
+    buscador.style.removeProperty("height");
+    buscador.style.borderRadius=("100%");
+    buscador.style.removeProperty("background");
+    buscador.style.removeProperty("color");
 });
+
+    
