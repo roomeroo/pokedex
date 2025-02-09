90a3391dbec467ebf9db9b8e16a9d4e86ec9410e
roomeroo05 <roomeroo05@gmail.com>
2025-02-09 18:06:02 +0100

    cambios subidos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let lupa = document.getElementById("lupa");
 let galeria = document.getElementById("contenedorPokemon");
+let numeroPag;
 
 async function recogerImagen(url){
     await fetch(url)
@@ -18,18 +19,27 @@ async function crearContenedores(){
             for(let i = 0; i < 20; i++){
                 let contenedor = document.createElement("div");
                 contenedor.classList.add("pokemon");
+                let contenedor2 = document.createElement("div");
+                contenedor2.classList.add("row");
                 let nombre = document.createElement("h2");
                 let imagen = document.createElement("img");
-                fetch(lista.results[i].url)
-                    .then(response => response.json())
-                    .then(pokemon => {
-                        imagen.src = pokemon.sprites.front_default;
-                        console.log(pokemon);
-                    });
                 nombre.textContent = lista.results[i].name;
                 contenedor.appendChild(imagen);
                 contenedor.appendChild(nombre);
+                contenedor.appendChild(contenedor2);
                 galeria.appendChild(contenedor);
+                fetch(lista.results[i].url)
+                .then(response => response.json())
+                .then(pokemon => {
+                    imagen.src = pokemon.sprites.front_default;
+                    for(let i = 0; i < pokemon.types.length; i++){
+                        let p = document.createElement("p");
+                        p.textContent = pokemon.types[i].type.name;
+                        p.classList.add(pokemon.types[i].type.name);
+                        p.classList.add("type");
+                        contenedor2.appendChild(p);
+                    }
+                });
             }
         })
 }
