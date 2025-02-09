1a7ee970238322646298f588131913be98e8eacf
Joseeaantoniio22 <jfergue055@g.educaand.es>
2025-02-09 19:32:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 let lupa = document.getElementById("lupa");
 let galeria = document.getElementById("contenedorPokemon");
 let numeroPag;
+let buscador = document.getElementById("buscador");
+let numeroVentana = 0;
 
-async function recogerImagen(url){
-    await fetch(url)
-        .then(response => response.json())
-        .then(pokemon => {
-            let img = document.createElement("img");
-            console.log(pokemon);
-            return img;
-        });
-}
+
+buscador.addEventListener("click", ()=>{
+    buscador.style.width=("150px")
+    buscador.style.height=("30px")
+    buscador.style.borderRadius=("10px")
+    buscador.style.background=("white")
+    buscador.style.transition=("all 0.5s linear")
+})
+
+buscador.addEventListener("blur", () => {       //blur => click fuera
+    buscador.style.removeProperty("width");
+    buscador.style.removeProperty("height");
+    buscador.style.borderRadius=("100%");
+    buscador.style.removeProperty("background");
+    buscador.style.removeProperty("color");
+});
 
 async function crearContenedores(){
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${numeroVentana*20}`)  //offset => inicio del array
         .then(response => response.json())
         .then(lista =>{
-            for(let i = 0; i < 20; i++){
+            for(let i = 0 + (numeroVentana * 20); i < 20 + (numeroVentana * 20); i++){
+                if(lista.length === 0)
+                    return;
                 let contenedor = document.createElement("div");
                 contenedor.classList.add("pokemon");
                 let contenedor2 = document.createElement("div");
                 contenedor2.classList.add("row");
                 let nombre = document.createElement("h2");
                 let imagen = document.createElement("img");
+                imagen.classList.add("imagenPokemon");
                 nombre.textContent = lista.results[i].name;
                 contenedor.appendChild(imagen);
                 contenedor.appendChild(nombre);
@@ -43,4 +55,19 @@ async function crearContenedores(){
             }
         })
 }
+
 crearContenedores();
+
+buscador.addEventListener("keyup", e =>{
+    
+    const busqueda = e.target.value.toLowerCase();
+    const pokemons = document.querySelectorAll(".pokemon");
+
+    pokemons.forEach(pokemon =>{
+        const nombre = pokemon.querySelector("h2").textContent.toLowerCase();
+        if(nombre.includes(busqueda)){
+            pokemon.style.display = "block"; // se muestra
+        }else
+            pokemon.style.display = "none"; // no se muestra
+    })
+})
